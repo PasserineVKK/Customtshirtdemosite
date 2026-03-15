20915aab0c64f218924f8e2cf6168a88b4c6bf82
Vang Khanh Khuyen <trinhnganquan@gmail.com>
2026-03-15 16:18:55 +0700

    Commit ignore

--- src/app/components/ui/starts-rating.tsx
+++ src/app/components/ui/starts-rating.tsx
@@ -7,4 +7,4 @@ export const StartsFiller = ({rating, startSize} : {rating:number, startSize: nu
                     <Star key={i} className={`${'w-'+startSize+' h-'+startSize}   ${(i<rating) ? 'fill-orange-400 text-orange-400' : 'fill-white-400 text-orange-400'}`} />
                   ))}
                 </div>)
-}
+} 
